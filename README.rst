223bf9670590347624550d5ea1621e6388df5e19
Outvi V <git@outv.im>
2019-01-13 11:29:53 +0800

    Initial commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+# django-impersonate-dj215
+
+Patched version of [django-impoersonate](https://pypi.org/project/django-impersonate/).
+
+Will be put down after [the pull request](https://bitbucket.org/petersanchez/django-impersonate/pull-requests/34) get merged.
---
+++ setup.py
@@ -0,0 +1,51 @@
+import os
+from distutils.core import setup
+
+project_name = 'impersonate'
+long_description = open('README.rst').read()
+
+# Idea from django-registration setup.py
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk(project_name):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.') or dirname == '__pycache__':
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[(len(project_name) + 1):]
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
+
+setup(
+    name='django-impersonate-dj215',
+    version=__import__(project_name).get_version(),
+    package_dir={project_name: project_name},
+    packages=packages,
+    package_data={project_name: data_files},
+    description='[Patched for Django 2.1.5+] Django app to allow superusers to impersonate other users.',
+    author='LibreHouse',
+    author_email='librehs@outv.im',
+    license='BSD License',
+    url='http://bitbucket.org/librehs/django-impersonate/',
+    long_description=long_description,
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Environment :: Web Environment',
+    ],
+)
